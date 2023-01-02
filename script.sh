33adec0ecc475022da94f2d1f7640645bc44bd18
Siska Stevani <44636088+siskastev@users.noreply.github.com>
2023-01-02 17:16:09 +0700

    add comment description container docker

--- script.sh
+++ script.sh
@@ -4,4 +4,5 @@ docker build -t stevanisiska/docker-list-pets .
 #docker build dengan menampilkan detail
 docker build -t stevanisiska/docker-list-pets --progress=plain --no-cache
 
+#create container docker
 docker container create --name docker-list-pets --publish 8181:8181 stevanisiska/docker-list-pets
